74b9f7596aec33f9be9d7f805271ec2f5319608b
AZ <awzack@gmail.com>
2021-08-31 21:06:12 -0500

    Cleaned up training script a little.

--- trainGan.py
+++ trainGan.py
@@ -11,17 +11,24 @@ import torchvision
 import torchvision.transforms.functional as TF
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-print(device)
+print(f"Using {device}")
 
-use_greyscale = True
-channels = 1 if use_greyscale else 3
-data_size = 15000
-
+# Model Design
+USE_GREYSCALE = False
 LATENT_SIZE = 192
-LAYER_SIZE = 128
-BATCH_SIZE = 16
+LAYER_SIZE = 192
 
-faceDS = FaceDataset("./img/input", greyscale=use_greyscale, size=data_size)
+# Training Details
+BATCH_SIZE = 32
+DATA_SIZE = 15000
+LEARNING_RATE = 0.001
+EPOCHS_PER_STEP = 8
+SCALE_STEPS = 4
+WRITE_EVERY_N = 50
+
+channels = 1 if USE_GREYSCALE else 3
+
+faceDS = FaceDataset("./img/input", greyscale=USE_GREYSCALE, size=DATA_SIZE)
 trainLoader = DataLoader(faceDS, batch_size=BATCH_SIZE, shuffle=True)
 print(f"Batches: {len(trainLoader)}")
 
@@ -32,19 +39,19 @@ gan = BaseGAN(LATENT_SIZE, 0.001, device)
 gan.setLoss(WassersteinLoss(sigmoid=False))
 
 generator = ProGen(latentDim=LATENT_SIZE, firstLayerDepth=LAYER_SIZE, outputDepth=channels)
-genOptim = AdamW(filter(lambda p: p.requires_grad, generator.parameters()), lr = 0.0001)
+genOptim = AdamW(filter(lambda p: p.requires_grad, generator.parameters()), lr=LEARNING_RATE)
 
 gan.setGen(generator, genOptim)
 
 discriminator = ProDis(firstLayerDepth=LAYER_SIZE, inputDepth=channels)
-disOptim = AdamW(filter(lambda p: p.requires_grad, discriminator.parameters()), lr = 0.0001)
+disOptim = AdamW(filter(lambda p: p.requires_grad, discriminator.parameters()), lr=LEARNING_RATE)
 
 gan.setDis(discriminator, disOptim)
 
-scheduler = ProGANScheduler(8, len(trainLoader), scale_steps=4)
+scheduler = ProGANScheduler(EPOCHS_PER_STEP, len(trainLoader), scale_steps=SCALE_STEPS)
 num_epochs = scheduler.get_max_epochs()
 
-print(gan.discriminator.num_params())
+print(f"Discriminator total initial weights: {gan.discriminator.num_params()}")
 
 # Training
 
@@ -78,7 +85,6 @@ if __name__ == "__main__":
             gan.gen_optimizer.add_param_group(new_gen_layers)
             gan.dis_optimizer.add_param_group(new_dis_layers)
 
-        tbStep = 0
 
         for i, data in enumerate(trainLoader):
             x = data.to(device)
@@ -92,8 +98,7 @@ if __name__ == "__main__":
 
             stepLossGen, outputs = gan.trainGen(BATCH_SIZE)
 
-            #if i % 10 == 9:
-            if True:
+            if j % WRITE_EVERY_N == 0:
 
                 obs = (1+j) * BATCH_SIZE    
 
@@ -104,7 +109,6 @@ if __name__ == "__main__":
                 if write_batch:
 
                     scale = trainLoader.dataset.getScale()
-                    print([grid.shape[1] * (64 // scale), grid.shape[2] * (64 // scale)])
                     resize_grid = TF.resize(
                         grid, 
                         [grid.shape[1] * (64 // scale), grid.shape[2] * (64 // scale)],
@@ -121,10 +125,9 @@ if __name__ == "__main__":
                 writer.add_image("output", grid, obs)
                 writer.add_scalar("loss_discriminator", stepLosses["total_loss"], obs)
                 writer.add_scalar("loss_generator", stepLossGen, obs)
-                #writer.add_scalar("grad_penalty", stepLosses["grad_loss"], obs)
-                #writer.add_scalar("non_grad_loss", stepLosses["non_grad_loss"], obs)
+                writer.add_scalar("grad_penalty", stepLosses["grad_loss"], obs)
+                writer.add_scalar("non_grad_loss", stepLosses["non_grad_loss"], obs)
                 #writer.add_scalar("real_dis_loss", stepLosses["dis_real"], obs)
                 #writer.add_scalar("fake_dis_loss", stepLosses["dis_fake"], obs)
 
-                tbStep += 1
-                j += 1
+            j += 1

0c9af56eb6e4186c6fd8f412f97fe5326385b856
Kevin Jiang <synsypa@users.noreply.github.com>
2021-11-03 21:40:45 -0700

    remove unnecessary import

--- faceDataset.py
+++ faceDataset.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, IterableDataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision.io import ImageReadMode
 import os
